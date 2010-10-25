431417a50ac2ce134db234658661f969115afa07
kataoka <kataoka@ubuntu9.(none)>
2010-10-25 16:47:17 +0900

    Updated testcase/infra

--- testcases/infra/CanonTest.cpp
+++ testcases/infra/CanonTest.cpp
@@ -1,25 +1,6 @@
-
 #include "Canon.h"
 #include "Tree.h"
 #include "TreePrinter.h"
-
-void printIR(tree::Stm *tree)
-{
-	tree::TreePrinter treePrinter;
-	if (tree) {
-		tree->accept(&treePrinter);
-	}
-	printf("%s\n", treePrinter.result.c_str());
-}
-
-void printStmList(const tree::StmList &slist)
-{
-	std::list<tree::Stm*>::const_iterator it = slist.li.begin();
-	while (it != slist.li.end()) {
-		printIR(*it);
-		++it;
-	}
-}
 
 int main(int argc, char **argv)
 {
@@ -35,9 +16,9 @@ int main(int argc, char **argv)
 	tree::ESEQ *eseq1 = _ESEQ(s1, eseq2);
 
 	tree::Stm *stm = _EXPR(eseq1);
-	printIR(stm);
+	tree::TreePrinter::printTree(stm);
 
 	tree::StmList slist;
 	slist = canon.linearize(stm);
-	printStmList(slist);
+	tree::TreePrinter::printStmList(slist);
 }
