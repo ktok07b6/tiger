89d26dda67f3d5d09f6354a70cff5ad47bf9cba1
Hiroaki Kataoka <kataoka@sinby.com>
2015-02-02 17:23:25 +0900

    support compiling a x86(32bit) binary on a x86-64 linux

--- cpp/tiger.mk
+++ cpp/tiger.mk
@@ -5,6 +5,8 @@ ifeq ($(TARGET), x86)
 INCS+=-I../gc/boehmgc/include
 LIBDIRS+=-L../gc/boehmgc/lib/$(TARGET)
 LIBS=-lgc
+OPTS=-m32 -g
+ASMOPTS=-32
 endif
 ifeq ($(TARGET), arm)
 TOOLCHAIN=~/bin/arm-2009q1
@@ -22,7 +24,7 @@ CXX=$(PREFIX)g++
 LD=$(PREFIX)ld
 
 all:obj/$(filename).S
-	$(SILENT) $(CC) -g $(INCS) -c runtime/runtime.c -o obj/runtime.o 
-	$(SILENT) $(AS) obj/$(filename).S -o obj/$(filename).o
-	$(SILENT) $(CC) -g $(LIBDIRS) -static -lc $(LIBGCC) obj/runtime.o  obj/$(filename).o $(LIBS) -o bin/$(filename)
+	$(SILENT) $(CC) $(OPTS) $(INCS) -c runtime/runtime.c -o obj/runtime.o 
+	$(SILENT) $(AS) $(ASMOPTS) obj/$(filename).S -o obj/$(filename).o
+	$(SILENT) $(CC) $(OPTS) $(LIBDIRS) -static -lc $(LIBGCC) obj/runtime.o  obj/$(filename).o $(LIBS) -o bin/$(filename)
 
